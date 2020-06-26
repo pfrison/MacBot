f0fb8ae6bac3208f5a91363bc1a98389a48b8e49
pfrison <pierre.frison@polytech-lille.net>
2020-06-27 01:30:51 +0200

    code cleaning / commeting + bot prefix is now a constant

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
+const botPrefix = "macpop"; // in fucking lower case you fucking monkey
+const helpCommand = "Command list :\n"
+		+ "\"" + botPrefix + " come\" : send the bot to your voice channel.\n"
+		+ "\"" + botPrefix + " leave\" : tell the bot to leave the voice channel.";
+
+var voiceConnection; // used to keep track of voice connection
+
+client.on("ready", () => {
   	console.log("Aye aye commander, I'm in.");
 });
 
-var voiceConnection;
-
-client.on('message', message => {
+client.on("message", message => {
 	if ( !message.content )
 		return;
-	const args = message.content.split(' ');
+	const args = message.content.split(" ");
 	
-	if ( args[0] === 'macpop' || args[0] === 'Macpop' || args[0] === 'MacPop' ) {
-		if ( args[1] === "help" )
-			message.channel.send('Command list :\n'
-					+ '"owo come" : send the bot to your voice channel.\n'
-					+ '"owo leave" : tell the bot to leave the voice channel.');
-		else if ( args[1] === 'come' ) {
+	// command handler. DO NOT FUCKING FORGET THE LOWER CASE
+	if ( args[0].toLowerCase() === botPrefix ) {
+		if ( args[1].toLowerCase() === "come" ) {
 			let wait = 0;
 			if ( voiceConnection ) {
 				disconnectFormCurrentVoiceChannel();
@@ -28,20 +30,26 @@ client.on('message', message => {
 				if ( message.member.voice.channel )
 					connectToVoiceChannel( message.member.voice.channel );
 				else
-					message.channel.send('Your not connected to any voice channel dumbass.\n'
-							+ 'You are fucking stupid and I hate you.');
+					message.channel.send("Your not connected to any voice channel dumbass.\n"
+							+ "You are fucking stupid and I hate you.");
 			}, wait);
-		} else if ( args[1] === 'leave' ) {
+		} else if ( args[1].toLowerCase() === "leave" ) {
 			if ( voiceConnection )
 				disconnectFormCurrentVoiceChannel();
 			else
-				message.channel.send('What the fuck, I am not connected to any voice channel.\n'
-					+ 'Leave me the fuck alone you piece of shit.');
+				message.channel.send("What the fuck, I am not connected to any voice channel.\n"
+						+ "Leave me the fuck alone you piece of shit.");
+		} else {
+			if ( args[1].toLowerCase() !== "help" ) {
+				message.channel.send("I don't speak idiot you useless cunt.\n"
+						+ "Please learn to use these commands you fucking savage monkey.");
+			message.channel.send(helpCommand);
 		}
 	}
 });
 
-client.on('guildMemberSpeaking', (member, speakingStatus) => {
+// Detect when a member speaking status change (talking / not talking)
+client.on("guildMemberSpeaking", (member, speakingStatus) => {
 	if ( member && speakingStatus ) {
 		const user = member.user;
 		console.log("Speaking event (" + speakingStatus + ") : " + user.username);
@@ -53,19 +61,19 @@ function connectToVoiceChannel( channel ) {
 		channel.join()
 				.then((connection) => {
 					voiceConnection = connection;
-					console.log("I'm in the voice channel. Scanning in progress.");
+					console.log("I"m in the voice channel. Scanning in progress.");
 				})
 				.catch((e) => { console.error(e); });
 	} else
 		console.error("Cannot get in the voice channel.");
 }
-
 function disconnectFormCurrentVoiceChannel() {
 	voiceConnection.disconnect();
 	voiceConnection = undefined;
 }
 
-process.on('SIGINT', function() {
+// PLEASE use this function to restore the bot state before you end the code LIKE A FUCKING SAVAGE WITH CRTL+C YOU FUCKING MONKEY
+process.on("SIGINT", function() {
     console.log("Caught interrupt signal captain ! Preparing to leave now.");
 	if ( voiceConnection )
 		disconnectFormCurrentVoiceChannel();
@@ -73,4 +81,5 @@ process.on('SIGINT', function() {
     process.exit();
 });
 
-client.login('NzI0NzU1MTI0MjIzNzM3ODc2.XvPuhw.kP0WGBMO6hp4LhglbuJIJfFIWEU');
+// Leave this at the end. If you don't leave his at the end of the code I will track you down and use your face to clean my windows. 
+client.login("NzI0NzU1MTI0MjIzNzM3ODc2.XvPuhw.kP0WGBMO6hp4LhglbuJIJfFIWEU");
